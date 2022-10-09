31df4e454752ae2e5145e323cf5b64b0ff740c26
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2022-10-09 22:49:40 +0700

    Belajar Route prefix, Group, middleware, Group didalam Group

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,73 @@ Route::get('/user/{id}/artikel/{artikelId}', function($id, $artikelId) {
 // ROUTE PARAMETER OPSIONAL 
 // UNTUK MEMBUAT ROUTE PARAMETER OPSIONAL 
 // WAJIB MENGGUNAKAN  TANDA TANYA (?) 
-Route::get('/dev/{id?}', function($id = null) {
+Route::get('/devs/{id?}', function($id = null) {
     return 'Developer Login ' . $id;
+});
+
+
+
+////////////// MEMBUAT ROUTE GROUP  //////////////////////
+// Route Group Default dengan prefix
+Route::prefix('user')->group(function() {
+    // definisikan Route 
+    Route::get('/change-password', function() {
+        return 'Change Password';
+    });
+    
+    Route::get('/profile', function() {
+        return 'Profile';
+    });
+    
+    Route::get('/photo', function() {
+        return 'Photo';
+    });
+});
+
+
+
+// Route Group dengan Tambahan Middleware auth 
+Route::get('/login', function() {
+    return 'Login Dahalu yak....';
+})->name('login');
+
+Route::prefix('dev')->middleware('auth')->group(function() {
+    // Definisikan Route
+    Route::get('/absen', function() {
+        return 'Absensi Developer';
+    });
+
+    Route::get('/data', function() {
+        return 'Basis Data Accunting';
+    });
+
+    Route::get('/audit', function() {
+        return 'Audit Data Barang';
+    });
+});
+
+
+
+// Route Group didalam Route Group
+Route::prefix('account')->group(function() {
+    // definisikan Route Group lainnya
+    Route::prefix('setting')->group(function() {
+        // Definisikan Route 
+        Route::get('/change-password', function() {
+            return 'Change-Password';
+        });
+
+        Route::get('/email', function() {
+            return 'Email';
+        });
+
+        Route::get('/photo', function() {
+            return 'Photo';
+        });
+    });
+
+    // Route diluar Route Group lainnya
+    Route::get('follower', function() {
+        return 'Follower';
+    });
 });
