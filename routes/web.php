ed5c26fee82b29e983ccbb87c5ab2a41638a4a86
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2022-10-09 21:22:15 +0700

    belajar Route parameter dan parameter opsional

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// MEMBUAT ROUTE DAN ROUTE APA SAJA PADA LARAVEL 
 
 // Route biasa 
-Route::get('/admin', function() {
-    return 'Admin';
-});
+// Route::get('/user', function() {
+//     return 'User';
+// });
 
 // Route Parameter
 Route::get('/admin/{id}', function($id) {
     return 'Admin ' . $id;
+});
+
+// ROUTE PARAMETER LEBIH DARI SATU 
+Route::get('/user/{id}/artikel/{artikelId}', function($id, $artikelId) {
+    return 'User ' . $id . " Artikel " . $artikelId;
+});
+
+// ROUTE PARAMETER OPSIONAL 
+// UNTUK MEMBUAT ROUTE PARAMETER OPSIONAL 
+// WAJIB MENGGUNAKAN  TANDA TANYA (?) 
+Route::get('/dev/{id?}', function($id = null) {
+    return 'Developer Login ' . $id;
 });
